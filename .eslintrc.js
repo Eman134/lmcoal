f3611a15a13927e8b1c767a6659710c81515c415
Eman134 <kaykeeman@gmail.com>
2024-05-14 21:46:51 -0300

    Implemented eslint, prettier and jest

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
+}
--- src/LMCoal.ts
+++ src/LMCoal.ts
@@ -1,27 +1,25 @@
 import fastify from 'fastify'
 
 export default class LMCoal {
+  constructor() {
+    console.log('Colecting coal...')
+  }
 
-    constructor() {
-        console.log('Colecting coal...')
-    }
+  ignite() {
+    const app = fastify()
 
-    ignite() {
-        const app = fastify()
+    const { ADDRESS = 'localhost', PORT = 3000 } = process.env
 
-        const { ADDRESS = 'localhost', PORT = 3000 } = process.env
+    app.get('/', async (_request, _reply) => {
+      return { hello: 'world' }
+    })
 
-        app.get('/', async (request, reply) => {
-            return { hello: 'world' }
-        })
-
-        app.listen({ host: ADDRESS, port: Number(PORT) }, (err, address) => {
-            if (err) {
-                console.error(err)
-                process.exit(1)
-            }
-            console.log(`Ignited at ${address}`)
-        })
-    }
-
+    app.listen({ host: ADDRESS, port: Number(PORT) }, (err, address) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      console.log(`Ignited at ${address}`)
+    })
+  }
 }
