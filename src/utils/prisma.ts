15d9116b61077c54b6cbe2a949c076a4cc586d3e
Eman134 <kaykeeman@gmail.com>
2024-06-22 20:10:14 -0300

    Setup prisma acceleration & optimize

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { withOptimize } from '@prisma/extension-optimize'
 const prisma = new PrismaClient()
+  .$extends(withAccelerate())
+  .$extends(withOptimize())
 export default prisma
